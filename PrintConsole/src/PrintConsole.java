dcdb2398566fb66228186b54493f7cd87531f3cd
Yann Kamche <yannkamche@gmail.com>
2023-04-03 16:08:59 +0100

    How to use the print and println commands in java

---
+++ PrintConsole/src/PrintConsole.java
@@ -0,0 +1,24 @@
+public class PrintConsole {
+    public static void main(String[] args) throws Exception {
+
+        // Prints Hello World
+        System.out.println("Hello, World!");
+
+        //Prints the statement in quotes and inserts a new line 
+        System.out.println("I am learning Java and the IDE  I use is VS code");
+
+        //Print does not insert a new line after a printing
+        System.out.print("I don't insert new lines after printed statement. ");
+
+        System.out.println("This text will be on the same line as the previous line");
+
+        System.out.println(3);
+        System.out.println(2000);
+        System.out.println(218.543);
+
+        System.out.println(3 + 3);
+        System.out.println(3 * 3);
+        System.out.println(10 * 2345);
+
+    }
+}
