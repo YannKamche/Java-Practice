b268924082a930063527c41923b541bf96d705b0
Yann Kamche <yannkamche@gmail.com>
2023-04-03 18:53:36 +0100

    Practicing Variables in Java

--- VariableManipulation/src/VariableManipulation.java
+++ VariableManipulation/src/VariableManipulation.java
@@ -13,5 +13,19 @@ public class VariableManipulation {
         for (int i = 0; i <= 10; i++) {
             System.out.println(5 + " * " + i + " = " + value * i);
         }
+
+        //Printing the content of these variables
+
+        int myAge = 20;
+        float myAgeFloat = 20.000f;
+        char myfirstLetter = 'K';
+        boolean amIStudent = true;
+        String myGreeting = "Hello";
+
+        System.out.println(myGreeting + "!!! My name is " + fullName + ". I am " + myAge + " years old.");
+        System.out.println("My age in float is " + myAgeFloat);
+        System.out.println("The first letter of my last name is " + myfirstLetter);
+        System.out.println("Am I a student? " + amIStudent );
+
     }
 }
