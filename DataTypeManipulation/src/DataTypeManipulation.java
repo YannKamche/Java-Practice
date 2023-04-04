c50bd01a2be871dc0ac413fb489357a17f817cb0
Yann Kamche <yannkamche@gmail.com>
2023-04-04 17:10:33 +0100

    Manipulations of primitive data types

---
+++ DataTypeManipulation/src/DataTypeManipulation.java
@@ -0,0 +1,27 @@
+public class DataTypeManipulation {
+    public static void main(String[] args) throws Exception {
+        //Manipulation Data Types
+
+        int myNum;
+        float myFloatNum;
+        char myChar;
+        boolean myBool;
+        String myString;
+
+        myNum = 24;
+        myFloatNum = 254.234f;
+        myChar = 'K';
+        myBool = true;
+        myString = "My name is Kamche Yann Arnaud";
+
+        System.out.println(myNum);
+        System.out.println(myFloatNum);
+        System.out.println(myChar);
+        System.out.println(myBool);
+        System.out.println(myString);
+
+        //Sum myNum and myFloatNum 
+
+        System.out.println(myNum + myFloatNum);
+    }
+}
