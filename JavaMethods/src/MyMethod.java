dff1ad4c6148b85237e6942dbf3d438ecde74d69
Yann Kamche <yannkamche@gmail.com>
2023-04-07 05:13:52 +0100

    Methods and Parameters

---
+++ JavaMethods/src/MyMethod.java
@@ -0,0 +1,11 @@
+public class MyMethod {
+    static void myGreetingMethod(String name, int age) {
+        System.out.println("Hello!! " + name);
+        System.out.println("You are : " + age + " years old");
+    }     
+
+    public static void main(String[] args) throws Exception {
+        myGreetingMethod("Yann", 18);  
+    }
+ }
+       
---
+++ JavaMethods/src/Sum.java
@@ -0,0 +1,8 @@
+public class Sum {
+    static int sum(int x, int y){
+        return x + y;
+    }
+    public static void main(String[] args) {
+        System.out.println(sum(5, 8));
+    }
+}
